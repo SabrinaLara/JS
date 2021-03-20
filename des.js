0f67f92566a14cb3bee8741a4361c75ab7284099
SabrinaLara <sabrinaaalejandra@gmail.com>
2021-03-20 20:30:09 -0300

    subida a git

---
+++ des.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingresa tu nombre");
+let numero1 = parseInt (prompt("Ingrese su edad"));
+let numero2 = parseInt (prompt("ingrese su número favorito"));
+console.log(nombre);
+alert("¡hola! " + nombre);
+alert("La suma de su edad más su número favorito es: " + (numero1 + numero2));
